59d8147be199fe91713e2e82661c66a7fd21eee0
SEO Book Pro <79488264+seobookpro@users.noreply.github.com>
2022-10-29 16:32:16 +0300

    Update header.php

--- header.php
+++ header.php
@@ -1,1 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- to add hook with the installation selected lang -->
 
+  <title>Blank WordPress SEO Custom Framework</title>
+<?php wp_head();?>
+</head>
+</body>
